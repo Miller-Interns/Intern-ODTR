aea15afaf67455c7861a1574a175ea74fb5a722a
Seth Cornelio <seth.cornelio@mllrdev.com>
2025-07-24 13:25:49 +0800

    chore: add seed script to add admin

---
+++ prisma/seed.js
@@ -0,0 +1,48 @@
+import { PrismaClient } from '../app/generated/prisma/index.js'
+
+const prisma = new PrismaClient();
+
+async function main() {
+    await prisma.user.createMany({
+        data: [
+            {
+				//Alyssa Palencia
+				id: '015084bc-bec3-4373-aec3-729fba0a825a',
+                email: 'alyssa.palencia@mllrdev.com',
+                name: 'Alyssa Palencia',
+				isAdmin: false,
+            },
+            {
+				//Karl Zablan
+				id: '0908da60-40f8-4b20-948c-dca792a64860',
+				email: 'karl.zablan@mllrdev.com',
+				name: 'Karl Zablan',
+				isAdmin: false,
+            },
+            {
+				//Seth Cornelio
+				id: 'cef438de-359c-4802-97ca-66d74dd50cf1',
+				email: 'seth.cornelio@mllrdev.com',
+				name: 'Seth Cornelio',
+				isAdmin: true,
+            },
+            {
+				//Sheen Balatero
+				id: 'ada24d94-f49e-4af1-91f0-64056ad149eb',
+				email: 'sheen.balatero@mllrdev.com',
+				name: 'Sheen Balatero',
+				isAdmin: false,
+			},
+        ]
+    })
+}
+
+main()
+	.then(async () => {
+		await prisma.$disconnect()
+	})
+	.catch(async (e) => {
+		console.error(e)
+		await prisma.$disconnect()
+		process.exit(1)
+	})
