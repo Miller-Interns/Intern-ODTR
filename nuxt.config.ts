f5c64d7884255269b1de9626aa4030ec3bb13347
Seth Cornelio <seth.cornelio@mllrdev.com>
2025-07-24 13:01:14 +0800

    chore: update models and prisma schema

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+import type { NuxtConfig } from "nuxt/config"
+
+const runtimeConfig: NuxtConfig['runtimeConfig'] = {
+	DATABASE_URL: process.env.POSTGRES_CONNECTION_URL,
+}
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+
+  runtimeConfig,
 
   modules: ["@prisma/nuxt",],
 })
