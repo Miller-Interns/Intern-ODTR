d2d8b345e13ccf80ec9dae69bc8e1b103c213b67
jie <remejiemaano17@gmail.com>
2025-08-12 16:26:51 +0800

    Task/6 login authentication (#13)
    * starting
    
    * update daghan
    
    * corrected
    
    * working(needs more work for auth)
    
    * error fixed
    
    * cleaned
    
    * cleaned and hashed seed passwords
    
    * for checking
    
    * ui: changed from css to nuxtui
    
    * ui: change UI according to the proposed UI
    
    * ui: removed the border of the log in form
    
    * ui: centered vertically the log in form
    
    * ui: fixed the * in UFormField of password and email
    
    * refactor: move login, logout, and user logic to usecase layer
    ui: display logged-in user's name on dashboard
    style: remove console logs from server files (user.get.ts{app/server/usecase} and auth.ts{app/middleware}).
    
    * refactor: changed the use case file paths and renamed all the use cases, also removed the auth folder.
    
    * refactor: made a type for paths {RouterNames.ts} and implemented it.
    
    * refactor: removed console logs from auth.ts and useGetUser.ts inside the server folder
    
    * wip: refactor whole structure
    
    * fix: login and logout
    
    ---------
    
    Co-authored-by: Seth Cornelio <seth.cornelio@mllrdev.com>

---
+++ app/pages/dashboard.vue
@@ -0,0 +1,69 @@
+<template>
+	<div
+		v-if="user"
+		class="dashboard-container"
+	>
+		<h1>Dashboard</h1>
+		<p>Welcome, hello!</p>
+		<p>This page is protected.</p>
+		<button
+			@click="handleLogout"
+			class="submit-button"
+		>
+			Log Out
+		</button>
+	</div>
+</template>
+
+<script setup lang="ts">
+	import { RouterNames } from '~/types/RouterNames'
+	import type { User } from '~/types/User'
+
+	const { user, fetch, clear } = useUserSession()
+	// const user = ref<User | null>(null)
+
+	onMounted(() => {
+		fetch()
+	})
+
+	const handleLogout = async () => {
+		try {
+			const response = await $fetch<{ status: boolean }>('/api/auth/logout', { method: 'POST' })
+			if (response.status) {
+				await clear()
+				navigateTo({ name: RouterNames.LOGIN }, { replace: true })
+			} else {
+				console.error('Logout failed: No response from server')
+			}
+		} catch (error) {
+			console.error('Logout failed:', error)
+		}
+	}
+</script>
+
+<style scoped>
+	.dashboard-container {
+		padding: 2rem;
+		font-family: sans-serif;
+		text-align: center;
+	}
+
+	.submit-button {
+		display: inline-block;
+		width: auto;
+		padding: 10px 20px;
+		font-size: 1rem;
+		font-weight: 600;
+		color: #ffffff;
+		background-color: #ef4444;
+		border: none;
+		border-radius: 8px;
+		cursor: pointer;
+		transition: background-color 0.2s;
+		margin-top: 24px;
+	}
+
+	.submit-button:hover {
+		background-color: #dc2626;
+	}
+</style>
